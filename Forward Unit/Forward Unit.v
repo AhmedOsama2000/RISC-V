06927e7b7ed17f4096c84be1a60f3b0abb3f3820
‪Ahmed Osama‬‏ <56164434+AhmedOsama2000@users.noreply.github.com>
2023-01-05 17:22:36 +0200

    Update Forward Unit.v

--- Forward Unit/Forward Unit.v
+++ Forward Unit/Forward Unit.v
@@ -5,8 +5,8 @@ module Forward_Unit #(
 	// INPUT
 	input  wire 				   EX_MEM_Reg_Wr,
 	input  wire 				   MEM_WB_Reg_Wr,
-	input  wire [WIDTH_SOURCE-1:0] ID_EX_src_1,
-	input  wire [WIDTH_SOURCE-1:0] ID_EX_src_2,
+	input  wire [WIDTH_SOURCE-1:0] ID_EX_Rs1,
+	input  wire [WIDTH_SOURCE-1:0] ID_EX_Rs2,
 	input  wire [WIDTH_SOURCE-1:0] EX_MEM_Rd,
 	input  wire [WIDTH_SOURCE-1:0] MEM_WB_Rd,
 
@@ -21,43 +21,43 @@ wire double_forward;
 
 assign EX_dest_x0 	  = !EX_MEM_Rd? 1'b1:1'b0;
 assign MEM_dest_x0 	  = !MEM_WB_Rd? 1'b1:1'b0;
-assign double_forward = (ID_EX_src_1 == ID_EX_src_2)? 1'b1:1'b0;
+assign double_forward = (ID_EX_Rs1 == ID_EX_Rs2)? 1'b1:1'b0;
 
 always @(*) begin
 	Forward_A = 2'b00;
 	Forward_B = 2'b00;
 	
 	// EX Forward
-	if (double_forward && !EX_dest_x0 && EX_MEM_Rd == ID_EX_src_1) begin
+	if (double_forward && !EX_dest_x0 && EX_MEM_Rd == ID_EX_Rs1) begin
 		
 		Forward_A = 2'b10;
 		Forward_B = 2'b10;
 
 	end
-	if (EX_MEM_Reg_Wr && !EX_dest_x0 && EX_MEM_Rd == ID_EX_src_1) begin
+	if (EX_MEM_Reg_Wr && !EX_dest_x0 && EX_MEM_Rd == ID_EX_Rs1) begin
 
 		Forward_A = 2'b10;
 		
 	end
-	else if (EX_MEM_Reg_Wr && !EX_dest_x0 && EX_MEM_Rd == ID_EX_src_2) begin
+	else if (EX_MEM_Reg_Wr && !EX_dest_x0 && EX_MEM_Rd == ID_EX_Rs2) begin
 
 		Forward_B = 2'b10;
 		
 	end
 
 	// MEM Forward
-	else if (double_forward && !MEM_dest_x0 && MEM_WB_Rd == ID_EX_src_1) begin
+	else if (double_forward && !MEM_dest_x0 && MEM_WB_Rd == ID_EX_Rs1) begin
 		
 		Forward_A = 2'b01;
 		Forward_B = 2'b01;
 
 	end
-	else if (MEM_WB_Reg_Wr && !MEM_dest_x0 && MEM_WB_Rd == ID_EX_src_1) begin
+	else if (MEM_WB_Reg_Wr && !MEM_dest_x0 && MEM_WB_Rd == ID_EX_Rs1) begin
 
 		Forward_A = 2'b01;
 		
 	end
-	else if (MEM_WB_Reg_Wr && !MEM_dest_x0 && MEM_WB_Rd == ID_EX_src_2) begin
+	else if (MEM_WB_Reg_Wr && !MEM_dest_x0 && MEM_WB_Rd == ID_EX_Rs2) begin
 
 		Forward_B = 2'b01;
 		
