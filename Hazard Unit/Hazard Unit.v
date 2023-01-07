0064f382591f8b63c278c54c3fab8398fb2eb680
‪Ahmed Osama‬‏ <56164434+AhmedOsama2000@users.noreply.github.com>
2023-01-07 18:08:22 +0200

    Update Hazard Unit.v

--- Hazard Unit/Hazard Unit.v
+++ Hazard Unit/Hazard Unit.v
@@ -8,56 +8,52 @@ module Hazard_Unit #(
 	input  wire [WIDTH_SOURCE-1:0] IF_ID_rs2,
 	input  wire [OPCODE_6_4-1:0]   opcode,
 	input  wire [WIDTH_SOURCE-1:0] ID_EX_Reg_rd,
-	input  wire 		       ID_EX_MEM_Rd, // Memory Read
+	input  wire 				   ID_EX_MEM_Rd, // Memory Read
 
 	// OUTPUT
-	output reg PC_Stall,
-	output reg IF_ID_Stall,
-	output reg Mux_Sel_Flush
+	output wire PC_Stall,
+	output wire IF_ID_Stall,
+	output wire Mux_Sel_Flush
 );
+
+localparam BRANCH = 3'b110;
+localparam ARITHM = 3'b011;
+localparam IMM    = 3'b001;
 
 wire expc_haz_1;
 wire expc_haz_2;
+reg [2:0] stall_flags;
 
 assign expc_haz_1 = ((IF_ID_rs1 == ID_EX_Reg_rd) && ID_EX_MEM_Rd)? 1'b1:1'b0;
 assign expc_haz_2 = ((IF_ID_rs2 == ID_EX_Reg_rd) && ID_EX_MEM_Rd)? 1'b1:1'b0;
 
+assign {PC_Stall,IF_ID_Stall,Mux_Sel_Flush} = stall_flags;
 
 always @(*) begin
 
-	PC_Stall      = 0;
-	IF_ID_Stall   = 0;
-	Mux_Sel_Flush = 0;
+	stall_flags = 3'b000;
 
 	// Stall in case of Branch
-	if (opcode == 3'b110 && (expc_haz_1 || expc_haz_2)) begin
+	if (opcode == BRANCH && (expc_haz_1 || expc_haz_2)) begin
 		
-		PC_Stall      = 1;
-		IF_ID_Stall   = 1;
-		Mux_Sel_Flush = 1;
+		stall_flags = 3'b111;
 
 	end
 	// Stall in case of Arithmatic or Mul or Div
-	else if (opcode == 3'b011 && (expc_haz_1 || expc_haz_2)) begin
+	else if (opcode == ARITHM && (expc_haz_1 || expc_haz_2)) begin
 		
-		PC_Stall      = 1;
-		IF_ID_Stall   = 1;
-		Mux_Sel_Flush = 1;
+		stall_flags = 3'b111;
 
 	end
 	// Stall in case of Imm
-	else if (opcode == 3'b001 && expc_haz_1) begin
+	else if (opcode == IMM && expc_haz_1) begin
 		
-		PC_Stall      = 1;
-		IF_ID_Stall   = 1;
-		Mux_Sel_Flush = 1;
+		stall_flags = 3'b111;
 
 	end
 	else begin
 		
-		PC_Stall      = 0;
-		IF_ID_Stall   = 0;
-		Mux_Sel_Flush = 0;
+		stall_flags = 3'b000;
 
 	end
 end
