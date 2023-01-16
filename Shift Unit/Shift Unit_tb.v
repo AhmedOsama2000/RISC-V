ef6fe4d79375c5fcd918abb443972ade6f8e5fd3
‪Ahmed Osama‬‏ <56164434+AhmedOsama2000@users.noreply.github.com>
2023-01-16 23:31:12 +0200

    Update Shift Unit_tb.v

--- Shift Unit/Shift Unit_tb.v
+++ Shift Unit/Shift Unit_tb.v
@@ -2,13 +2,17 @@ module Shift_Unit_tb;
 
 	parameter XLEN   = 32;
 
-	reg [XLEN-1:0]   Src1;
+	reg signed [XLEN-1:0]   Src1;
 	reg [5:0]   	 Src2;
 	reg  			 funct3_2; 
 	reg              funct7_5;
 	reg              En;
 
 	wire [XLEN-1:0]  Result;
+
+
+	wire [1:0] funct_test;
+	reg [XLEN-1:0] expected_result;
 
 	Shift_Unit DUT (
 		.Src1(Src1),
@@ -19,7 +23,12 @@ module Shift_Unit_tb;
 		.Result(Result)
 	);
 
+	localparam SLL = 2'b00;
+	localparam SRL = 2'b01;
+	localparam SRA = 2'b11;
 	integer i;
+
+	assign funct_test = {funct7_5,funct3_2};
 
 	initial begin
 
@@ -34,19 +43,21 @@ module Shift_Unit_tb;
 		En   = 1;
 		Src1 = 50;
 		Src2 = 4;
+		check_result(Src1,Src2);
 
 		// SRL
 		#5
 		funct3_2 = 1;
 		Src1 	 = 32'hABCDFFFF;
 		Src2 	 = 5;
+		check_result(Src1,Src2);
 
 		// SRA
 		#5
 		funct7_5 = 1;
 		Src1 	 = 32'hABCDFFFF;
 		Src2 	 = 3;
-
+		check_result(Src1,Src2);
 		#5
 
 		for (i = 0;i < 10;i = i + 1) begin
@@ -55,11 +66,32 @@ module Shift_Unit_tb;
 			Src2 	 = $random;
 			funct3_2 = $random;
 			funct7_5 = $random;
-			#5;
+			#5
+			check_result(Src1,Src2);
 		end
 
 		$stop;
 
 	end
+
+	task check_result(input signed [XLEN-1:0] Src1 ,input [4:0] Src2);
+		begin
+			#2
+			case (funct_test)
+				SLL: expected_result = Src1 << Src2;
+				SRL: expected_result = Src1 >> Src2;
+				SRA: expected_result = Src1 >>> Src2;
+				default: expected_result = 'b0;
+			endcase
+			if (expected_result === Result) begin
+				$display("At time %0t:Result = %0d = %0d",$time,Result,expected_result);
+			end
+			else begin
+				$display("At time %0t:Result = %0d not = %0d",$time,Result,expected_result);
+			end
+
+		end
+	endtask
+
 
 endmodule
