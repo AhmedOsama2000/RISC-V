ce1316601399c41eec45f2953d881056495cbc95
‪Ahmed Osama‬‏ <56164434+AhmedOsama2000@users.noreply.github.com>
2023-01-16 23:42:35 +0200

    Update Logic Unit_tb.v

--- Logic Unit/Logic Unit_tb.v
+++ Logic Unit/Logic Unit_tb.v
@@ -9,6 +9,12 @@ module Logical_Unit_tb;
 
 	wire [XLEN-1:0]  Result;
 
+	reg  [XLEN-1:0] expected_result;
+
+	localparam AND = 2'b11;
+	localparam OR  = 2'b10;
+	localparam XOR = 2'b00;
+
 	Logical_Unit DUT (
 		.Src1(Src1),
 		.Src2(Src2),
@@ -31,11 +37,31 @@ module Logical_Unit_tb;
 			Src1 	   = $random;
 			Src2 	   = $random;
 			funct3_1_0 = $random;
+			check_result(Src1,Src2);
 			#5;
 		end
 
 		$stop;
 
 	end
+
+	task check_result(input [XLEN-1:0] Src1 ,input [XLEN-1:0] Src2);
+		begin
+			#2
+			case (funct3_1_0)
+				AND: expected_result = Src1 & Src2;
+				OR: expected_result = Src1 | Src2;
+				XOR: expected_result = Src1 ^ Src2;
+				default: expected_result = 'b0;
+			endcase
+			if (expected_result === Result) begin
+				$display("At time %0t:Result = %0d = %0d",$time,Result,expected_result);
+			end
+			else begin
+				$display("At time %0t:Result = %0d not = %0d",$time,Result,expected_result);
+			end
+
+		end
+	endtask
 
 endmodule
