ebc907a74821f3d99fe17fec371204b3a80975ec
‪Ahmed Osama‬‏ <56164434+AhmedOsama2000@users.noreply.github.com>
2023-02-17 16:04:11 +0200

    Update CLA_Adder.v

--- CLA_ADD_SUB/CLA_Adder.v
+++ CLA_ADD_SUB/CLA_Adder.v
@@ -3,8 +3,8 @@ module CLA_ADD_SUB
   	parameter WIDTH = 4
 )
 (
-	input  wire [WIDTH-1:0] rs_1,
-	input  wire [WIDTH-1:0] rs_2,
+	input  wire [WIDTH-1:0] Rs1,
+	input  wire [WIDTH-1:0] Rs2,
 	input  wire             En,
 	input  wire             funct7_5,
 	output wire [WIDTH-1:0] result,
@@ -17,7 +17,7 @@ wire [WIDTH-1:0] Pg;
 wire [WIDTH-1:0] Sum;
 wire [WIDTH-1:0] passed_rs2; 
 
-assign passed_rs2 = (funct7_5 && En)? ~rs_2:rs_2;
+assign passed_rs2 = (funct7_5 && En)? ~Rs2:Rs2;
 
 // Create the Full Adders
 genvar full_adder_gen;
@@ -26,7 +26,7 @@ generate
 
 		// Output Carry is not needed but generated via the CLA_Generator
 		FA full_adder_insts ( 
-			.A0(rs_1[full_adder_gen]),
+			.A0(Rs1[full_adder_gen]),
 			.B0(passed_rs2[full_adder_gen]),
 			.C0(Carry[full_adder_gen]),
 			.En(En),
@@ -41,13 +41,14 @@ genvar             gen_c;
 generate
 	for (gen_c = 0; gen_c < WIDTH; gen_c = gen_c + 1) begin
 
-	    assign G[gen_c]  			= rs_1[gen_c] & passed_rs2[gen_c] & En;
-	    assign Pg[gen_c]  		= (rs_1[gen_c] | passed_rs2[gen_c]) & En;
+	    assign G[gen_c]  			= Rs1[gen_c] & passed_rs2[gen_c] & En;
+	    assign Pg[gen_c]  		= (Rs1[gen_c] | passed_rs2[gen_c]) & En;
 	    assign Carry[gen_c+1] = (G[gen_c] | (Pg[gen_c] & Carry[gen_c])) & En;
 
 	 end
 endgenerate
    
+
   // Carry Depend on ADD/SUB Operation
   assign Carry[0] = funct7_5;
  
