30d701a7065342e76e27d6e63e19fbd38e8beda6
‪Ahmed Osama‬‏ <56164434+AhmedOsama2000@users.noreply.github.com>
2023-07-05 02:26:36 +0300

    Fixing Extending Values

--- Integration/IMM EXT/Imm_Ext.v
+++ Integration/IMM EXT/Imm_Ext.v
@@ -24,19 +24,19 @@ localparam JALR    	 = 5'b11001;
 always @(*) begin
 	case (opcode)
 		JAL: begin
-			IMM_OUT = {{11{1'b0}},IMM_IN[31],IMM_IN[19:12],IMM_IN[20],IMM_IN[30:21],1'b0};
+			IMM_OUT = {{11{IMM_IN[31]}},IMM_IN[31],IMM_IN[19:12],IMM_IN[20],IMM_IN[30:21],1'b0};
 		end
 		LUI , AUIPC: begin
 			IMM_OUT = {IMM_IN[31:12], {12{1'b0}}};
 		end
 		ARITM_IMM , JALR, LOAD_I , LOAD_F: begin
-			IMM_OUT = {{11{1'b0}},IMM_IN[31:20]};
+			IMM_OUT = {{20{IMM_IN[31]}},IMM_IN[31:20]};
 		end
 		BRANCH: begin
-			IMM_OUT = {{19{1'b0}},IMM_IN[31],IMM_IN[7],IMM_IN[30:25],IMM_IN[11:8],1'b0};
+			IMM_OUT = {{19{IMM_IN[31]}},IMM_IN[31],IMM_IN[7],IMM_IN[30:25],IMM_IN[11:8],1'b0};
 		end
 		STORE_I , STORE_F: begin
-			IMM_OUT = {{20{1'b0}},IMM_IN[31:25],IMM_IN[11:7]};
+			IMM_OUT = {{20{IMM_IN[31]}},IMM_IN[31:25],IMM_IN[11:7]};
 		end
 		default: IMM_OUT = 32'b0;
 	endcase
